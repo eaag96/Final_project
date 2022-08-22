e3389e09979fb6b93a2d8630fe5a568bac542733
esraa0096 <esraa.gramah96@outlook.com>
2022-08-22 17:33:40 +0300

    Added view & urls files and some html pages

---
+++ base/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import TaskCreate, TaskDetail, TaskList
+from base import views
+
+urlpatterns = [
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+]
